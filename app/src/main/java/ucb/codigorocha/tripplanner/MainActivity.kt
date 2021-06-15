219cfaa1964edcac1fc8bc067ca6722b7c74c973
vrochacaio <vrocha.caio98@gmail.com>
2021-06-15 19:15:43 -0300

    FINISH

--- app/src/main/java/ucb/codigorocha/tripplanner/MainActivity.kt
+++ app/src/main/java/ucb/codigorocha/tripplanner/MainActivity.kt
@@ -5,6 +5,10 @@ import android.content.Intent
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Html
+import android.text.Html.FROM_HTML_MODE_LEGACY
+import android.text.Spanned
+import android.text.method.LinkMovementMethod
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -20,5 +24,8 @@ class MainActivity : AppCompatActivity() {
         btn_myPlans.setOnClickListener {
             startActivity(Intent(this@MainActivity, MyPlansActivity::class.java))
         }
+
+        val styledText: Spanned = Html.fromHtml(getString(R.string.acesse_o_google), FROM_HTML_MODE_LEGACY)
+        txtView_google.setMovementMethod(LinkMovementMethod.getInstance());
     }
 }
